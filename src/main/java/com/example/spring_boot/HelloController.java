b199ebf039d1b0af079bc81a3af5455de109547c
yash raj <yashraj0207@outlook.com>
2024-06-09 01:29:16 +0530

    initial setup

---
+++ src/main/java/com/example/spring_boot/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(){
+        return  "Greetings! Hello World!";
+    }
+}
